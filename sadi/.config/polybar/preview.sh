89f7ffebf7fca99d97698751d648d08a711be681
sad1go0 <sayidullohabbasov0909@gmail.com>
2022-08-23 03:47:34 +0500

    updated sadi's configs

--- sadi/.config/polybar/preview.sh
+++ sadi/.config/polybar/preview.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# Add this script to your wm startup file.
-
 DIR="$HOME/.config/polybar"
 
 # Terminate already running bar instances
@@ -10,5 +8,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch the bar
-polybar -q main -c "$DIR"/config.ini &
+# Launch the preview bar
+polybar -q top -c "$DIR"/preview.ini &
+polybar -q mid -c "$DIR"/preview.ini &
+polybar -q bottom -c "$DIR"/preview.ini &
